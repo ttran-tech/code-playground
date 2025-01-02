ded3309e5270ff630540f8984c6e4a7788888db4
Tan Tran <duytantran2710@gmail.com>
2025-01-02 17:54:40 +0000

    minor update api_server, rename payload function and add GET method process

--- new2025/api_server/api.py
+++ new2025/api_server/api.py
@@ -2,18 +2,19 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-@app.route('/payload1', methods=['GET','POST'])
-def payload1():
+@app.route('/payload', methods=['GET','POST'])
+def payload():
     if request.method == 'GET':
         return "Web API"
     else:
         data = request.json
-        timezone = data.get('timezone')
+        pid = data.get('pid')
 
-        if timezone:
-            print(f'Receive timezone: {timezone}')
+        if pid:
+            print(f'Receive pid: {pid}')
+        return "Data Received"
 
 
 
 if __name__ == "__main__":
-    app.run(port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
