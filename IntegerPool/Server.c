6919dfcf2a8746d2f286f77ed21db9c23cee3d03
Tan Tran <duytantran2710@gmail.com>
2025-02-04 16:22:25 +0000

    implement print_request() to display the request data

--- IntegerPool/Server.c
+++ IntegerPool/Server.c
@@ -23,6 +23,12 @@ void handle_request(Request request)
 
 }
 
+void print_request(Request request)
+{
+    printf("Process ID: %d\nRequest Type: %d\nValue: %d\n", request.process_id, request.request_type, request.value);
+}
+
+
 int main()
 {
     int fd;
@@ -31,13 +37,15 @@ int main()
     Request request;
     LList *pool_list = llist_init();
 
+    printf("Server started.\n");
     while (TRUE)
     {
-        printf("Waiting for request...");
+        printf("Waiting for request...\n");
         
         // Handle read end
         fd = open(FIFO_PATH, O_RDONLY);
         read(fd, &request, sizeof(request));
+        print_request(request);
         handle_request(request);
         close(fd);
 
