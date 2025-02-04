a454e6c8c1ad81217c97768e6fda3695f3801093
Tan Tran <duytantran2710@gmail.com>
2025-02-04 22:33:36 +0000

    modify the handle_request statement to print out the return value

---
+++ IntegerPool/Test.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#include "Pool.h"
+#include "Request.h"
+#include "Common.h"
+
+extern LList *pool_list;
+
+int main()
+{
+    int value;
+
+    Request request;
+    request.process_id = getpid();
+    request.request_type = REQUEST_NUMBER;
+    request.value = 5;
+
+    pool_list = llist_init();
+    value = handle_request(request, pool_list);
+
+    printf("Returned value = %d\n", value);
+
+    return 0;
+}
