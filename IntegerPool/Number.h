e103706ff61f5d7c00c66dda3930dc94ef6b5cd5
Tan Tran <duytantran2710@gmail.com>
2025-02-07 21:29:43 +0000

    Refactor: move NumberNode to Number file for separating the NumberNode function from Client code

---
+++ IntegerPool/Number.h
@@ -0,0 +1,15 @@
+#ifndef number_h
+#define number_h
+
+#include "LList.h"
+
+typedef struct NumberNode {
+    int value;
+    struct NumberNode *next;
+} NumberNode;
+
+// NumberNode operations
+NumberNode * NumberNode_search(LList *llist, int value);
+int NumberNode_has_value(LList *llist, int value);
+
+#endif
