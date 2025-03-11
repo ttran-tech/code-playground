7e3fd667163a964375070f2fad422e697a0e1023
Tan Tran <duytantran2710@gmail.com>
2025-03-11 14:15:36 +0000

    Feat: for testing purpose

--- python/date_generate.py
+++ python/date_generate.py
@@ -1,4 +1,5 @@
 from datetime import datetime, timedelta
+from typing import List, Dict
 from itertools import filterfalse
 import re, traceback, random, json
 
@@ -11,65 +12,80 @@ class FormatError(Exception):
     pass
 
 
-def generate_commit_dates(start_date:str, end_date:str, min_active_days_per_week:int, max_active_days_per_week:int, start_hour:int, end_hour:int, min_commit_per_day:int, max_commit_per_day:int) -> dict[str, list[str]]:
+def generate_commit_dates(start_date:str, end_date:str, min_active_days_per_week:int, max_active_days_per_week:int, start_hour:int, end_hour:int, min_commit_per_day:int, max_commit_per_day:int) -> Dict[str, List[str]]:
     """Generate a commit dates dictionary and pair each entry with a list commit hours."""
+    
+    def _generate_active_dates(start_date:str, end_date:str, min_active_days_per_week:int, max_active_days_per_week:int) -> List[str]:
+        """Generate a list of active dates"""
+        DATE_REGEX_PATTERN = r'^(\d{4})-(\d{2})-(\d{2})$'
+        if not bool(re.match(DATE_REGEX_PATTERN, start_date)) or not bool(re.match(DATE_REGEX_PATTERN, end_date)):
+            raise FormatError('Invalid date format. Date format must match "YYYY-MM-DD". Example: 2025-01-01')
+        
+        if start_date == end_date:
+            raise FormatError('Invalid date range: The start date and end date cannot be the same')
+
+        DATE_FORMAT = '%Y-%m-%d'
+        start_date = datetime.strptime(f"{start_date}", DATE_FORMAT)
+        end_date = datetime.strptime(f"{end_date}", DATE_FORMAT)
+
+        # Get the number of weeks between the start and end date
+        week_total = ((end_date - start_date).days) // DAYS_PER_WEEK
+        print(f" [+] Week Total: {week_total}")
+
+        # active_date = []
+        active_dates = set()
+        next_start_date = start_date
+        for i in range(week_total):
+            active_date_per_week = random.randint(min_active_days_per_week, max_active_days_per_week)
+            next_end_date = next_start_date + timedelta(days=DAYS_PER_WEEK)
+            dates = [(next_start_date + timedelta(days=n)) for n in range((next_end_date - next_start_date).days + 1)]
+            for j in range(active_date_per_week):
+                # Use filterfalse to eliminate the elements already in active_dates, 
+                # and create a list of available dates
+                available_dates = list(filterfalse(active_dates.__contains__, dates))
+                if available_dates:
+                    date = random.choice(available_dates).strftime(DATE_FORMAT)
+                    active_dates.add(date)
+            next_start_date = next_end_date
+        active_dates = sorted(active_dates)
+        return active_dates
+
+    def _generate_commit_hours(active_dates:List[datetime], start_hour:int, end_hour:int, min_commit_per_day:int, max_commit_per_day:int) -> Dict[str,List[str]]:
+        """Generate commit hours per date"""
+        commit_dates = {}
+        for active_date in active_dates:
+            commit_hours = []
+            commit_per_day = random.randint(min_commit_per_day, max_commit_per_day-1) # exlude the last hour
+            for i in range(commit_per_day):
+                hour = random.randint(start_hour, end_hour)
+                minute = random.randint(0, 59)
+                second = random.randint(0, 59)
+                commit_hours.append(f"{hour:02d}:{minute:02d}:{second:02d}")
+            commit_hours.sort()
+            commit_dates[active_date] = commit_hours
+        return commit_dates
+    
     try:
-        active_dates = generate_active_dates(start_date, end_date, min_active_days_per_week, max_active_days_per_week)
-        commit_dates = generate_commit_hours(active_dates, start_hour, end_hour, min_commit_per_day, max_commit_per_day)
+        active_dates = _generate_active_dates(start_date, end_date, min_active_days_per_week, max_active_days_per_week)
+        commit_dates = _generate_commit_hours(active_dates, start_hour, end_hour, min_commit_per_day, max_commit_per_day)
         return commit_dates
     except FormatError as e:
         traceback.print_exc()
-
-
-def generate_active_dates(start_date:str, end_date:str, min_active_days_per_week:int, max_active_days_per_week:int) -> list[str]:
-    """Generate a list of active dates"""
-    DATE_REGEX_PATTERN = r'^(\d{4})-(\d{2})-(\d{2})$'
-    if not bool(re.match(DATE_REGEX_PATTERN, start_date)) or not bool(re.match(DATE_REGEX_PATTERN, end_date)):
-        raise FormatError('Invalid date format. Date format must match "YYYY-MM-DD". Example: 2025-01-01')
-    
-    DATE_FORMAT = '%Y-%m-%d'
-    start_date = datetime.strptime(f"{start_date}", DATE_FORMAT)
-    end_date = datetime.strptime(f"{end_date}", DATE_FORMAT)
-    week_total = int(((end_date - start_date).days) / DAYS_PER_WEEK)  # get the number of weeks between the start and end date
-
-    # active_date = []
-    active_dates = set()
-    next_start_date = start_date
-    for i in range(week_total):
-        active_date_per_week = random.randint(min_active_days_per_week, max_active_days_per_week)
-        next_end_date = next_start_date + timedelta(days=DAYS_PER_WEEK)
-        dates = [(next_start_date + timedelta(days=n)) for n in range((next_end_date - next_start_date).days + 1)]
-        for j in range(active_date_per_week):
-            # Use filterfalse to eliminate the elements already in active_dates, 
-            # and create a list of available dates
-            available_dates = list(filterfalse(active_dates.__contains__, dates))
-            if available_dates:
-                date = random.choice(available_dates).strftime(DATE_FORMAT)
-                active_dates.add(date)
-        next_start_date = next_end_date
-    active_dates = sorted(active_dates)
-    return active_dates
-
-
-def generate_commit_hours(active_dates:list[datetime], start_hour:int, end_hour:int, min_commit_per_day:int, max_commit_per_day:int) -> dict[str,list[str]]:
-    """Generate commit hours per date"""
-    commit_dates = {}
-    for active_date in active_dates:
-        commit_hours = []
-        commit_per_day = random.randint(min_commit_per_day, max_commit_per_day-1) # exlude the last hour
-        for i in range(commit_per_day):
-            hour = random.randint(start_hour, end_hour)
-            minute = random.randint(1, 60)
-            second = random.randint(1, 60)
-            commit_hours.append(f"{hour:02d}:{minute:02d}:{second:02d}")
-        commit_hours.sort()
-        commit_dates[active_date] = commit_hours
-    return commit_dates
+        return None
 
 
 if __name__ == '__main__':
-    commit_dates = generate_commit_dates('2023-01-01', '2023-03-01', 2, 5, 8, 17, 1, 8)
+    start_date = '2023-01-01'
+    end_date = '2023-02-01'
+    min_active_day_per_week = 2
+    max_active_day_per_week = 5
+    start_hour = 8
+    end_hour = 17
+    min_commit_per_day = 1
+    max_commit_per_day = 5
+    commit_dates = generate_commit_dates(start_date, end_date, min_active_day_per_week, max_active_day_per_week, start_hour, end_hour, min_commit_per_day, max_commit_per_day)
     
+    print(f"\t Total commit_dates: {len(commit_dates.keys())}")
     if commit_dates:
         commit_dates_json = json.dumps(commit_dates, indent=2)
         print(commit_dates_json)
---
+++ python/git_push.py
@@ -0,0 +1,10 @@
+import subprocess
+
+
+REPO_URL = 'https://github.com/ttran-tech/git-contribot-test.git'
+
+def test_git_push():
+    pass
+
+if __name__ == '__main__':
+    test_git_push()
